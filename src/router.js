3b4d752b920490461dc26f8e0d9defd85e97a1d1
camicasii <camicasii@hotmail.com>
2020-01-04 18:42:20 -0400

    add cloudinary

--- src/router.js
+++ src/router.js
@@ -1,28 +1,51 @@
-import { Router } from 'express';
-import pool from './database/database';
-const router =  Router()
+import { Router } from "express";
+import pool from "./database/database";
+const router = Router();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOAD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-router.get("/upload",(req,res)=>{
-    console.log(req.file);
-    
-    return res.json({hola:123})
+router.get("/upload", (req, res) => {
+  console.log(req.file);
 
-})
-router.post("/upload",(req,res)=>{
-    console.log(req.file);
-    
-    return res.json({hola:123})
+  return res.json({ hola: 123 });
+});
+router.post(
+  "/upload",
+  (req, res, next) => {
+    if (req.file !== undefined) return next();
+    return res.json({ state: 401 });
+  },
+  async (req, res) => {
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "test-cloudinary.app"
+    });
 
-})
-router.get('/',async (req,res)=>{
-    const row = await pool.query('SELECT * FROM CONTABILIDAD');
-    console.log(row);
-    
+    const { title, description } = req.body;
+    const { filename, originalname, mimetype, size, path } = req.file;
+`"UPDATE CONTABILIDAD set \`URL\` ="dfsfasdfsaf"  WHERE ID=18;"`
+    const newimage = {
+      title,
+      description,
+      filename,
+      originalname,
+      mimetype,
+      size,
+      path,
+      url: result.url,
+      public_id: result.public_id
+    };
+  }
+);
 
-    res.json({hola:row})
-})
+router.get("/", async (req, res) => {
+  const row = await pool.query("SELECT * FROM CONTABILIDAD");
+  console.log(row);
 
-export  default router;
+  res.json({ hola: row });
+});
 
-
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,41 +1,29 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { graphqlExpress ,ApolloServer,GraphQLUpload} from 'apollo-server-express';
-import morgan from 'morgan';
+import express from "express";
+import bodyParser from "body-parser";
+import { ApolloServer } from "apollo-server-express";
+import morgan from "morgan";
 import typeDefs from "./schema2";
-import resolvers from './resolve'
-import './database/database';
-import router from './router'; 
-import path from 'path'
+import resolvers from "./resolve";
+import "./database/database";
+import router from "./router";
+import path from "path";
+import multer from "multer";
+const uuid = require('uuid/v4')
 const app = express();
-import multer from  'multer'
-app.set('port',process.env.PORT||4001)
-//Middlewere
-app.use(morgan('dev'))
-app.use( bodyParser.json())
+app.set("port", process.env.PORT || 4001);
 
 const storage = multer.diskStorage({
-    destination:(req,file,cb)=>cb(null,path.join(__dirname,'/uploads')),
-    filename:(req,file,cb)=>cb(null,file.originalname.concat('-',Date.now())),
-    
-    
-})
-
-var upload = multer({storage}).single("upload")
-
-app.use(upload)
-
-app.use('/',router)
-
-
-const server = new ApolloServer({ typeDefs, resolvers,
-context:(req)=>{
-    
-    
-
-}});
-
-server.applyMiddleware({app,cors:true
-})
-
-app.listen(app.get('port'),()=>console.log("server on port",app.get('port')))
+  destination: (req, file, cb) => cb(null, path.join(__dirname, "/uploads")),
+  filename: (req, file, cb) =>
+    cb(null, uuid()+path.extname(file.originalname))
+});
+var upload = multer({ storage }).single("upload");
+//Middlewere
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use("/", upload, router);
+const server = new ApolloServer({ typeDefs, resolvers, context: req => {} });
+server.applyMiddleware({ app, cors: true });
+app.listen(app.get("port"), () =>
+  console.log("server on port", app.get("port"))
+);
